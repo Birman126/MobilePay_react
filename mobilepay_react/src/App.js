1f3fc1c611b4ba9ba9837f089b65fe583de93263
Birman <birman126@gmail.com>
2022-02-02 11:21:51 +0400

    add operators

--- mobilepay_react/src/App.js
+++ mobilepay_react/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Operator from './Operator/Operator.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    operator: [
+      {name: 'Beline'},
+      {name: 'Megafon'},
+      {name: 'Tele2'}
+    ],
+    pageTitle: 'Выберите оператора'
+  }
+changeTitleHandler () {
+  console.log('click')
+}
+  render() {
+    const divStyle = {
+      textAlign: 'center'
+    }
+
+    const operator = this.state.operator
+
+    return (
+      <div style={divStyle}>
+        <h1>{this.state.pageTitle}</h1>
+        <button onClick ={this.changeTitleHandler}>Ololo</button>
+        <Operator name={operator[0].name}  /> 
+        <Operator name={operator[1].name}  />
+        <Operator name={operator[2].name}  />
+      </div>
+    );
+  }
 }
 
 export default App;
